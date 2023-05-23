0d22ba63ce72903463ae05cf3c667019f1a6c8cf
Ksenya <ksenya.nikitchenko.03@mail.ru>
2023-05-23 23:12:48 +0300

    f

--- index.php
+++ index.php
@@ -93,22 +93,26 @@ else{
   else{
     setcookie('email_value',$_POST['email'],time()+30*24*60*60);
   }
-  if($_POST['year']!=''){
-    setcookie('year_value','',time()+30*24*60*60);
+  if($_POST['year']==''){
+    setcookie('year_error','',time()+24*60*60);
+    $errors=TRUE;
+  }
+  else{
+    setcookie('year_value',$_POST['year'],time()+30*24*60*60);
   }
   if(empty($_POST['gender'])){
     setcookie('gender_error','',time()+24*60*60);
     $errors=TRUE;
   }
   else{
-    setcookie('gender_value','',time()+30*24*60*60);
+    setcookie('gender_value',$_POST['gender'],time()+30*24*60*60);
   }
   if(empty($_POST['limbs'])){
     setcookie('limbs_error','',time()+24*60*60);
     $errors=TRUE;
   }
   else{
-    setcookie('limbs_value','',time()+30*24*60*60);
+    setcookie('limbs_value',$_POST['limbs'],time()+30*24*60*60);
   }
   if(empty($_POST['biography'])){
     setcookie('biography_error','',time()+24*60*60);

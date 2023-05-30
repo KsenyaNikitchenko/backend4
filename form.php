864168fda4b1d90017d3fc4708b520871d0bea1f
Ksenya <ksenya.nikitchenko.03@mail.ru>
2023-05-30 20:33:17 +0300

    f

--- form.php
+++ form.php
@@ -19,7 +19,7 @@ if (!empty($messages)) {
 ?>
     <div class="form">
         <h1>Сверхспособности</h1>
-        <form action="index.php" method="POST">
+        <form action="" method="POST">
             <label for="name">Введите имя:<br>
                 <input name="name" placeholder="Введите имя" <?php if (!empty($errors['name'])) {print 'class="error"';} ?>
             <?php if(empty($errors['name'])&&!empty($values['name'])){print 'class="ok"';}?> value='<?php if(!empty($values['name'])) print $values['name'];?>'><br>
